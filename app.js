4a3443cce57dcec023051825f828d1dd686a8426
Wanjiku Njoroge <njorogewanjiku265@gmail.com>
2021-03-17 17:30:32 +0300

    added styles,images and linked pages

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require ("express");
 
 const app = express()
 
+app.use(express.static("public"));
+
 app.set('views','./views')
 app.set('view engine','ejs')
 
 app.get('/', (req,res) => {
-   res.render("index",{name:"vitu fishi"})
+   res.render("index")
 } );
+
+app.get('/items',(req,res)=>{
+   res.render("items")
+})
 
 
 
