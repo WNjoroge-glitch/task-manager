bf405f00f8913be043ef96befb449373e8629166
Wanjiku Njoroge <njorogewanjiku265@gmail.com>
2021-03-18 17:04:23 +0300

    created list and respective pages

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ app.get('/', (req,res) => {
 app.get('/items',(req,res)=>{
    res.render("items")
 })
-
+app.get('/potatoes',(req,res) => {
+   res.render('potatoes')
+})
+app.get('/chilli',(req,res) => {
+   res.render('chilli')
+})
+app.get('/yams',(req,res) => {
+   res.render("yams")
+})
 
 
 
