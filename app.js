9b19c1f99b92b1420e4667f5731ddd905f8e2a92
Wanjiku Njoroge <njorogewanjiku265@gmail.com>
2021-03-26 17:44:32 +0300

    added form and pushed new item to list

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 
 app.use(express.static("public"));
 
+//config to get access to form values
+app.use(express.urlencoded({extended:false}))
+
 app.set('views','./views')
 app.set('view engine','ejs')
 
@@ -17,11 +20,25 @@ app.get('/items',(req,res)=>{
    res.render("items",{ items:items })
 })
 
+//grab form
+
+
 app.get('/create',(req,res) => {
-   res.render('add');
+   res.render('create');
 })
 
+//submit form
 
+app.post('/create',(req,res) => {
+   //grab new item & add to the list
+   
+let count = items.length + 1
+items.push({id:count,name:req.body.newItem})
+
+
+  //redirect to items page
+  res.redirect('items')
+})
 
 
 
